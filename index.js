1d497c6cd85843c433b3c5020c2d6e28e53312c3
Mera Stackhouse <mera.stackhouse@gmail.com>
2019-01-16 00:08:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ function shout(string) {
   return string.toUpperCase()
 }
 
-fucntion whisper(string) {
+function whisper(string) {
   return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
 }
